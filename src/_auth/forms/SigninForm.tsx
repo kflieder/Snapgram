f3e52866110b7baadff3a034c70976c7ca68b1c5
kflieder <kacey.flieder@gmail.com>
2024-02-15 20:32:01 -0600

    Initial commit

---
+++ src/_auth/forms/SigninForm.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+
+const SigninForm = () => {
+  return (
+    <Button>Click me</Button>
+  )
+}
+
+export default SigninForm
---
+++ src/components/ui/shared/Loader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div>
+        <img src="/assets/images/loader.svg" alt="Loader" width={24} height={24}/>
+    </div>
+  )
+}
+
+export default Loader
