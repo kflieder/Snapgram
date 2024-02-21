55253d808b7ce9e1c2de9baaf0502fc3601960c1
kflieder <kacey.flieder@gmail.com>
2024-02-20 19:41:53 -0600

    create post
    added forms to the create post tab

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import PostForm from '@/components/forms/PostForm'
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
+    <div className="flex flex-1">
+      <div className="common-container">
+        <div className="max-w5xl flex-start gap-3 justify-start w-full">
+          <img src="/icons/add-post.svg"
+          width={36}
+          height={36}
+          alt="add"
+          />
+          <h2 className="h3-bold md:h2-bold text-left w-full">Create Post</h2>
+        </div>
+        <PostForm />
+      </div>
+    </div>
   )
 }
 
