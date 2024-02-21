b900754485058579d9e1b35b07eca42657b50353
kflieder <kacey.flieder@gmail.com>
2024-02-20 18:34:14 -0600

    Routes and NavBars
    created the different routes for each page, created each page, and created a side nav bar for desktop and a bottombar for mobile

--- src/components/ui/shared/Topbar.tsx
+++ src/components/ui/shared/Topbar.tsx
@@ -2,10 +2,12 @@ import { useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { Button } from '@/components/ui/button'
 import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations'
+import { useUserContext } from '@/context/AuthContext'
 
 const Topbar = () => {
     const navigate = useNavigate();
     const { mutate: signOut, isSuccess } = useSignOutAccount()
+    const { user } = useUserContext();
 
     useEffect(() => {
         if (isSuccess) navigate(0);
@@ -21,6 +23,12 @@ const Topbar = () => {
                 <Button variant="ghost" className="shad-button_ghost" onClick={() => signOut()}>
                     <img src='/icons/logout.svg' alt="logout" />
                 </Button>
+                <Link to={'/profile/${user.id}'} className="flex-center gap-3">
+                    <img src={user.imageUrl || '/assets/images/profile-placeholder.svg'}
+                    alt="profile"
+                    className="h-8 rounded-full"
+                    />
+                </Link>
             </div>
         </div>
     </section>
